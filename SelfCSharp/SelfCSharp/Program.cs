a87a5ff983d838a57a2c20f0d45f3e66953aebe5
asukamekaru98 <asuka.m2298oki@gmail.com>
2023-11-24 23:37:29 +0900

    独習C# 1章

---
+++ SelfCSharp/SelfCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("あなたの名前は？");
+            string name = Console.ReadLine();
+            Console.WriteLine("こんにちは、{0}さん!", name);
+        }
+    }
+}
